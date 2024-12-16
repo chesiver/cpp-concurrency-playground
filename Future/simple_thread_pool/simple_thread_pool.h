ede2fbf6d4310d6b68358b53e840ab95a4f40c86
chesiver <ljhchesiver@gmail.com>
2024-12-16 14:08:55 -0800

    update simple thread pool to use jthread

--- Future/simple_thread_pool/simple_thread_pool.h
+++ Future/simple_thread_pool/simple_thread_pool.h
@@ -26,31 +26,30 @@ class thread_pool {
 
    private:
     // need to keep track of threads so we can join them
-    std::vector<std::thread> workers;
+    std::vector<std::jthread> _workers;
     // the task queue
     std::queue<std::function<void()>> _tasks;
     // synchronization
-    std::mutex queue_mutex;
-    std::condition_variable _condition;
-    bool _stop;
+    std::mutex _queue_mutex;
+    std::condition_variable_any _condition;
     // for waiting for all tasks
     std::atomic<int> _running_tasks_num{0};
     std::atomic<bool> _task_completed{false};
 };
 
-// the constructor just launches some amount of workers
-thread_pool::thread_pool(size_t threads) : _stop(false) {
+// the constructor just launches some amount of _workers
+thread_pool::thread_pool(size_t threads) {
     for (size_t i = 0; i < threads; i += 1)
-        workers.emplace_back([this] {
-            for (;;) {
+        _workers.emplace_back([this] (std::stop_token stop_token){
+            while (true) {
                 std::function<void()> task;
                 {
-                    std::unique_lock<std::mutex> lock(this->queue_mutex);
-                    _condition.wait(lock, [this] {
-                        return _stop || !this->_tasks.empty();
+                    std::unique_lock<std::mutex> lock(_queue_mutex);
+                    _condition.wait(lock, stop_token, [this] {
+                        return !_tasks.empty();
                     });
-                    if (_stop && _tasks.empty()) {
-                        return;
+                    if (stop_token.stop_requested()) {
+                        break;
                     }
                     task = std::move(_tasks.front());
                     _tasks.pop();
@@ -59,13 +58,14 @@ thread_pool::thread_pool(size_t threads) : _stop(false) {
                 if (prev_running_tasks_num == 0) {
                     _task_completed.store(false);
                 }
-                task();
+                std::invoke(task);
                 _running_tasks_num.fetch_sub(1);
                 if (_running_tasks_num.load() == 0) {
                     _task_completed.store(true);
                     _task_completed.notify_one();
                 }
             }
+            std::cout << "end of worker thread\n";
         });
 }
 
@@ -78,11 +78,7 @@ auto thread_pool::enqueue(F&& f, Args&&... args)
         std::bind(std::forward<F>(f), std::forward<Args>(args)...));
     std::future<return_type> res = task->get_future();
     {
-        std::unique_lock<std::mutex> lock(queue_mutex);
-        // don't allow enqueueing after stopping the pool
-        if (_stop) {
-            throw std::runtime_error("enqueue on stopped thread_pool");
-        }
+        std::unique_lock<std::mutex> lock(_queue_mutex);
         _tasks.emplace([task]() { (*task)(); });
     }
     _condition.notify_one();
@@ -95,15 +91,10 @@ auto thread_pool::wait_for_all() -> void {
     }
 }
 
-// the destructor joins all threads
-thread_pool::~thread_pool() {
-    {
-        std::unique_lock<std::mutex> lock(queue_mutex);
-        _stop = true;
-    }
-    _condition.notify_all();
-    for (std::thread& worker : workers) {
-        worker.join();
+simple_thread_pool::thread_pool::~thread_pool() {
+    wait_for_all();
+    for (auto &worker: _workers) {
+        worker.request_stop();
     }
 }
 
